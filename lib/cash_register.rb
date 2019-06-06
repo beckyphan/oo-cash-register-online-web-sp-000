ce704301951f0e6101d7c6feca53f8085c56df56
Becky Phan <beckyphan@live.com>
2019-06-06 02:11:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,68 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :cart
+  
+  def initialize(discount = nil)
+    @total = 0 
+    @discount = discount
+    @cart = []
+    @itemized_list = []
+  end 
+
+  
+  def total
+  # @cart = [[title, price, qty], [title1, price1, qty1]
+    if @cart.size == 0 
+      nil
+    else
+      @cart.each do |item|
+        if item.size == 2 
+          @total += (item[1] * 1)
+        elsif item.size == 3
+          @total += (item[1] * item[2])
+        end
+      end
+    end
+    @total
+  end 
+  
+  
+  def add_item(title, price, qty = nil)
+    qty == nil ? @cart << [title, price] : @cart << [title, price, qty]
+  end
+
+
+  def apply_discount
+    if self.discount != nil
+      subtotal = self.total
+      grand_total = subtotal - (@discount.to_f / 100 * subtotal)
+      puts "After the discount, the total comes to #{grand_total}."
+    else 
+      puts "There is no discount to apply."
+    end
+  end
+  
+  
+  def items 
+    @cart.each do |item|
+      if item.length == 2 
+        @itemized_list << item[0] 
+      elsif item.length == 3 
+        counter = item[2]
+        while counter > 0
+          @itemized_list << item[0]
+          counter -= 1
+        end
+      end
+    end 
+    @itemized_list
+  end 
+
+  
+  def void_last_transaction
+    @cart.pop
+    self.total
+  end
+  
+end
