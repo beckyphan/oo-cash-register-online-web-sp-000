2fc28ec865e1e26e3e927f4b69d7ca4eda23be25
Becky Phan <beckyphan@live.com>
2019-06-06 02:28:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,12 +34,13 @@ class CashRegister
 
 
   def apply_discount
-    if self.discount != nil
-      subtotal = self.total
-      grand_total = subtotal - (self.discount.to_f / 100 * subtotal)
-      puts "After the discount, the total comes to #{grand_total}."
-    else 
+    if self.discount == nil
       puts "There is no discount to apply."
+    else 
+      subtotal = self.total
+      discounted = self.discount.to_f / 100 * subtotal
+      grand_total = subtotal - discounted
+      puts "After the discount, the total comes to #{grand_total}."
     end
   end
   
